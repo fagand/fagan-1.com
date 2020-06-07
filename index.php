a99a184a2bc2d212d191eb98f512a2ddcc12f2b5
fagand <47316903+fagand@users.noreply.github.com>
2020-06-07 16:46:53 +0100

    bringing back the music

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html lang="en">
+
+<head>
+    <title>David Fagan | @iamfagan | fagan-1.com</title>
+    <?php include 'header.html'; ?>
+</head>
+
+<body class="is-preload">
+
+    <!-- Sidebar -->
+    <section id="sidebar">
+        <div class="inner">
+            <nav>
+                <ul>
+                    <li><a href="#intro">Welcome</a></li>
+                    <li><a href="#history">History</a></li>
+                    <li><a href="#sites">Old Sites</a></li>
+                    <li><a href="#mixtapes">The MixTapes</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </section>
+
+    <!-- Wrapper -->
+    <div id="wrapper">
+
+        <!-- Intro -->
+        <section id="intro" class="wrapper style1 fullscreen fade-up">
+            <div class="inner" style="margin: 30vh 0 30vh 0">
+                <h1>fagan-1.com</h1>
+                <p>This site utilises your devices display settings. If your device is in 'dark mode' 🌒 you will see a dark site and vice versa for 'light mode'! 💡</p>
+                <ul class="actions">
+                    <li><a href="#history" class="button scrolly">Click here to learn more</a></li>
+                    <li><a href="#mixtapes" class="button scrolly">Click here to go to The Mixtapes</a></li>
+                </ul>
+            </div>
+        </section>
+
+        <!-- One -->
+        <section id="history" class="wrapper style2 spotlights">
+            <section>
+                <a href="#history" class="image scrolly"><img src="images/pic01.jpg" alt="" data-position="top center" /></a>
+                <div class="content">
+                    <div class="inner">
+                        <h2>History</h2>
+                        <p>Fagan-1.Com has been my site for many years. In the past, the site was very popular and attracted lots of visitors to the sight seeking music. The Internet Archive below shows the site popularity between from 2006 onwards. The site has been maintained by me throughout this time.</p>
+                        <p>Between 2006-2009 the site was one of the most popular in Scotland to people of the same age as me. The main draw of the website was the availability of the then popular PCDJ music. My site was the most updated, reliable at the time hence it’s popularity. </p>
+                        <p>Nowadays there is no demand for the music and the website has been rarely visited for a few years. The only people I imagine will come across this site will be people who were familiar with it from back in the day. For anyone unfamiliar you can check the web archive of the site here.</p>
+                        <p>I would like to make the music available again. As yet, I haven't worked out the best way of doing that. Unlike before, the music wouldn't be downloaded, it would have to be some form of streaming service which opens it up to copyright claims etc.</p>
+                        <ul class="actions">
+                            <li><a href="http://web.archive.org/web/2019*/www.fagan-1.com" target="_blank" class="button">Internet Archive</a></li>
+                            <li><a href="https://davidfagan.co.uk" target="_blank" class="button">Portfolio</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </section>
+
+        <section id="sites" class="wrapper style2 spotlights">
+            <section>
+                <a href="javascript:;" class="image scrolly"><img src="images/siteimages/site1.png" alt="" data-position="top center" /></a>
+                <div class="content">
+                    <div class="inner">
+                        <h2>The Original '.TK' Site! (2005-2006)</h2>
+                        <p>This was where it started for me. There was a lot of people at the time making a '.TK' site. I decided to get involved and made my own design – I think this one was made using <a href="https://en.wikipedia.org/wiki/Adobe_Fireworks">Macromedia Fireworks.</a></p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <a href="javascript:;" class="image scrolly"><img src="images/siteimages/site2.png" alt="" data-position="top center" /></a>
+                <div class="content">
+                    <div class="inner">
+                        <h2>The First 'Fagan-1.Com' Site! (2006)</h2>
+                        <p>This was my first site with the domain and hosting. I designed the site in Photoshop and if I remember right, I used Fireworks for the slicing again. Some of my inspiration for this one came from EliteNick.Com, at the time he was promoting some good web designs and features within his site. things such as the affiliates and web stats I took from there.</p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <a href="javascript:;" class="image scrolly"><img src="images/siteimages/site3.png" alt="" data-position="top center" /></a>
+                <div class="content">
+                    <div class="inner">
+                        <h2>Trying New Designs... (2006)</h2>
+                        <p>At this point I was experimenting with new designs. I wasn't thinking this at the time - it just happened as a coincidence - but this one was more user friendly, with all of the content in the middle and very little distractions.</p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <a href="javascript:;" class="image scrolly"><img src="images/siteimages/site4.png" alt="" data-position="top center" /></a>
+                <div class="content">
+                    <div class="inner">
+                        <h2>Nowadays this would be reffered to as <a href="https://en.wikipedia.org/wiki/Light-on-dark_color_scheme">'Dark Mode'</a> (2007)</h2>
+                        <p>I liked the design of this one. I kept the blue theme going but this time I changed from light to dark and I think it was cool. The glow around the navigation was 👎 but apart from that I think the colours went well. This site was larger in resolution compared to the rest, giving me more room to display content.</p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <a href="javascript:;" class="image scrolly"><img src="images/siteimages/site5.png" alt="" data-position="top center" /></a>
+                <div class="content">
+                    <div class="inner">
+                        <h2>Final Design (2007-2008)</h2>
+                        <p>This site was my favourite mix of design and functionality. The content was split over two sections this time, with navigation above each box. The main draw of the site was the music (Tunes) and that got its own section on the home page. This way, users when they went on the site could see the latest music and download it quickly.</p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <a href="javascript:;" class="image scrolly"><img src="images/siteimages/xmas.png" alt="" data-position="top center" /></a>
+                <div class="content">
+                    <div class="inner">
+                        <h2>Christmas Site</h2>
+                        <p>This site was a take on the second design with some added festive cheer! I would change the appearance of the site during the festive period for fun.</p>
+                    </div>
+                </div>
+            </section>
+        </section>
+
+        <!-- Two -->
+        <section id="mixtapes" class="wrapper style1 fade-up">
+            <div class="inner">
+                <h2>The Mixtapes</h2>
+                <p>Although it's not possible to make every track available like before; going forward, tracks will be available in the form of a mixtape. These will feature artists selected at random.</p>
+                <p>In the future, requests to feature on a mixtape <b>may</b> be available. To read more about featuring on a mixtape: <a href="/mixtapes/requests.html">click here</a>. Archived tracks available to be featured can be viewed <a href="mixtapes/archivelist.html">here</a>.</p>
+                <div class="features">
+                    <section>
+                        <span class="icon solid major fa-music"></span>
+                        <h3>Mixtape One - Promoting the movement of <a href="https://twitter.com/hashtag/blacklivesmatter?lang=en">#BlackLivesMatter</a></h3>
+                        <p>Coming soon</p>
+                        <p><a href="mixtapes/">Click Here</a></p>
+                    </section>
+                    <!--
+                    <section>
+                        <span class="icon solid major fa-lock"></span>
+                        <h3>Aliquam sed nullam</h3>
+                        <p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+                    </section>
+                    <section>
+                        <span class="icon solid major fa-cog"></span>
+                        <h3>Sed erat ullam corper</h3>
+                        <p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+                    </section>
+                    <section>
+                        <span class="icon solid major fa-desktop"></span>
+                        <h3>Veroeros quis lorem</h3>
+                        <p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+                    </section>
+                    <section>
+                        <span class="icon solid major fa-link"></span>
+                        <h3>Urna quis bibendum</h3>
+                        <p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+                    </section>
+                    <section>
+                        <span class="icon major fa-gem"></span>
+                        <h3>Aliquam urna dapibus</h3>
+                        <p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+                    </section>
+-->
+                </div>
+            </div>
+        </section>
+
+
+        <!-- Three -->
+        <section id="contact" class="wrapper style1 fade-up">
+            <div class="inner">
+                <h2>Get in touch</h2>
+                <p>Use the contact form below to send me an email.</p>
+                <div class="split style1">
+                    <section>
+                        <form action="action_page.php" method="post">
+                            <div class="fields">
+                                <div class="field half">
+                                    <label for="name">Name</label>
+                                    <input type="text" name="Name" id="name" required />
+                                </div>
+                                <div class="field half">
+                                    <label for="email">Email</label>
+                                    <input type="email" name="Email" id="email" required />
+                                </div>
+                                <div class="field">
+                                    <label for="message">Message</label>
+                                    <textarea name="Message" id="message" rows="5"></textarea>
+                                    <br>
+                                    <div class="g-recaptcha" data-sitekey="6LdoWPQUAAAAAA7R0Wgz3hNZFJOaYUpKsJT5QEPp"></div>
+                                </div>
+                            </div>
+                            <ul class="actions">
+                                <li><button type="submit" name="submit" class="button">
+                                        <i class="fa fa-paper-plane"></i> SEND MESSAGE
+                                    </button></li>
+                                <li><input type="reset" value="Reset" /></li>
+                            </ul>
+                        </form>
+                    </section>
+                    <section>
+                        <ul class="contact">
+                            <li>
+                                <h3>Email</h3>
+                                <a href="mailto:admin@fagan-1.com">admin@fagan-1.com</a>
+                            </li>
+                            <li>
+                                <h3>Social</h3>
+                                <ul class="icons">
+                                    <li><a href="http://twitter.com/iamfagan" class="fa-twitter"><span class="label">Twitter</span></a></li>
+                                    <li><a href="http://linkedin.com/in/iamfagan" class="fa-linkedin"><span class="label">LinkedIn</span></a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </section>
+                </div>
+            </div>
+        </section>
+
+    </div>
+
+    <!-- Footer -->
+    <footer id="footer" class="wrapper style1-alt">
+        <div class="inner">
+            <ul class="menu">
+                <li>&copy; David Fagan. All rights reserved.</li>
+                <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+            </ul>
+            <ul class="menu">
+                <li>Images: UnSplash::
+                    <a href="https://unsplash.com/@josef_photography" target="_blank">Josef_Photography</a> & <a href="https://unsplash.com/@rupixen" target="_blank">Rupixen</a></li>
+            </ul>
+        </div>
+    </footer>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.scrollex.min.js"></script>
+    <script src="assets/js/jquery.scrolly.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js?v=1"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- Vertical ad -->
+    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3755930033410564" data-ad-slot="8750553256" data-ad-format="auto" data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</body></html>
